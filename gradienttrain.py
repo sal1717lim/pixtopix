48d93f44160711d0f93c5351d73f09d670bf7f6c
SALIM <saissi1701@gmail.com>
2022-05-21 11:24:03 +0100

    gradient model

--- gradienttrain.py
+++ gradienttrain.py
@@ -147,7 +147,7 @@ def test_fn(
             if sys.argv[2] == "L1":
                 L1 = l1_loss(y_fake, y) * int(sys.argv[3])
             else:
-                L1 = (1 - l1_loss((y_fake.type(torch.DoubleTensor) + 1) / 2, (y.type(torch.DoubleTensor) + 1) / 2)) * int(sys.argv[3])
+                L1 = (1 - l1_loss((y_fake.type(torch.double()) + 1) / 2, (y.type(torch.double()) + 1) / 2)) * int(sys.argv[3])
             G_loss = G_fake_loss + L1
             resultat.append(L1.item())
 
